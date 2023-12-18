20a3020d3a8573bda960c19e71161df8c7e6214b
shhdh <90132007+ShahidHameed4@users.noreply.github.com>
2023-12-18 20:57:21 +0500

    error handling for routes

--- appointments/app.py
+++ appointments/app.py
@@ -1,60 +1,77 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
-import os
+from pymongo.errors import PyMongoError
 
 app = Flask(__name__)
 
-
+# MongoDB configuration
 app.config['MONGO_URI'] = "mongodb://mongo-service:27017/appointments"
-
-
 mongo = PyMongo(app)
 
-@app.route('/hello')
-def hello():
-  greeting = "Hello world!"
-  return greeting
-
+# Sample appointments data
 sample_appointments = [
-{ 'id': "1",'doctor': "1", 'date': "21 Nov 2023", 'rating':"Good"  },
-{ 'id': "2",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-{ 'id': "3",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
-{ 'id': "4",'doctor': "1", 'date': "22 Nov 2023", 'rating':"Bad"  },
-{ 'id': "5",'doctor': "2", 'date': "22 Nov 2023", 'rating':"Good"  },
+    {'id': "1", 'doctor': "1", 'date': "21 Nov 2023", 'rating': "Good"},
+    {'id': "2", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
+    {'id': "3", 'doctor': "2", 'date': "22 Nov 2023", 'rating': "Good"},
+    {'id': "4", 'doctor': "1", 'date': "22 Nov 2023", 'rating': "Bad"},
+    {'id': "5", 'doctor': "2", 'date': "22 Nov 2023", 'rating': "Good"},
 ]
 
 # Insert sample data
-mongo.db.appointments.insert_many(sample_appointments)
+try:
+    mongo.db.appointments.insert_many(sample_appointments)
+except PyMongoError as e:
+    print(f"Error inserting sample data: {e}")
+
+@app.route('/hello')
+def hello():
+    greeting = "Hello world!"
+    return greeting
 
 @app.route('/appointments', methods=["GET"])
 def getAppointments():
-    appointments = list(mongo.db.appointments.find())
+    try:
+        appointments = list(mongo.db.appointments.find())
 
-    # Convert ObjectId to string in each appointment
-    for appointment in appointments:
-        appointment['_id'] = str(appointment['_id'])
+        # Convert ObjectId to string in each appointment
+        for appointment in appointments:
+            appointment['_id'] = str(appointment['_id'])
 
-    return jsonify(appointments)
+        return jsonify(appointments)
 
+    except PyMongoError as e:
+        return jsonify({'error': f"Error retrieving appointments: {e}"}), 500
 
 @app.route('/appointments/', methods=["POST"])
 def addAppointment():
-  appointment = {
-    "id": request.json['id'],
-    "doctor_id": request.json['doctor_id'],
-    "patient_id": request.json['patient_id'],
-    "date": request.json['date'],
-    "time": request.json['time']
-  }
-  mongo.db.appointments.insert(appointment)
-  return jsonify(appointment)
+    try:
+        appointment = {
+            "id": request.json.get('id'),
+            "doctor_id": request.json.get('doctor_id'),
+            "patient_id": request.json.get('patient_id'),
+            "date": request.json.get('date'),
+            "time": request.json.get('time')
+        }
 
+        mongo.db.appointments.insert_one(appointment)
+        return jsonify(appointment)
 
+    except PyMongoError as e:
+        return jsonify({'error': f"Error adding appointment: {e}"}), 500
 
 @app.route('/appointment/<id>', methods=["GET"])
 def getAppointment(id):
-    appointment = mongo.db.appointments.find_one({"id": id})
-    return jsonify(appointment)
+    try:
+        appointment = mongo.db.appointments.find_one({"id": id})
+        if appointment:
+            # Convert ObjectId to string
+            appointment['_id'] = str(appointment['_id'])
+            return jsonify(appointment)
+        else:
+            return jsonify({'error': 'Appointment not found'}), 404
+
+    except PyMongoError as e:
+        return jsonify({'error': f"Error retrieving appointment: {e}"}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7070)
--- doctors/app.py
+++ doctors/app.py
@@ -1,19 +1,24 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
-import os
+from pymongo.errors import PyMongoError
 
 app = Flask(__name__)
 
+# MongoDB configuration
 app.config['MONGO_URI'] = "mongodb://mongo-service:27017/doctors"
-
 mongo = PyMongo(app)
+
+# Sample doctors data
 sample_doctors = [
-{ 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality':"DevOps"  },
-{ 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality':"Test"  }
+    {'id': "1", 'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality': "DevOps"},
+    {'id': "2", 'firstName': "Good", 'lastName': "Doctor", 'speciality': "Test"},
 ]
 
 # Insert sample data
-mongo.db.doctors.insert_many(sample_doctors)
+try:
+    mongo.db.doctors.insert_many(sample_doctors)
+except PyMongoError as e:
+    print(f"Error inserting sample data: {e}")
 
 @app.route('/hello')
 def hello():
@@ -22,18 +27,31 @@ def hello():
 
 @app.route('/doctors', methods=["GET"])
 def getDoctors():
-    doctors = list(mongo.db.doctors.find())
-    # Convert ObjectId to string in each doctor
-    for doctor in doctors:
-        doctor['_id'] = str(doctor['_id'])
-    return jsonify(doctors)
+    try:
+        doctors = list(mongo.db.doctors.find())
+
+        # Convert ObjectId to string in each doctor
+        for doctor in doctors:
+            doctor['_id'] = str(doctor['_id'])
+
+        return jsonify(doctors)
+
+    except PyMongoError as e:
+        return jsonify({'error': f"Error retrieving doctors: {e}"}), 500
 
 @app.route('/doctor/<id>', methods=["GET"])
 def getDoctor(id):
-    doctor = mongo.db.doctors.find_one({"id": id})
-    return jsonify(doctor)
+    try:
+        doctor = mongo.db.doctors.find_one({"id": id})
+        if doctor:
+            # Convert ObjectId to string
+            doctor['_id'] = str(doctor['_id'])
+            return jsonify(doctor)
+        else:
+            return jsonify({'error': 'Doctor not found'}), 404
 
-
+    except PyMongoError as e:
+        return jsonify({'error': f"Error retrieving doctor: {e}"}), 500
 
 @app.route('/health')
 def health():
